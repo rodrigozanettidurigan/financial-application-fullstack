121fc2ef3fb6a6bc496cb0aae681c7f65b98db6b
Rodrigo <rodrigozdurigan@gmail.com>
2026-05-11 21:59:32 -0300

    Pessoas FIX

--- src/app/features/pessoas/services/pessoa.ts
+++ src/app/features/pessoas/services/pessoa.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap} from 'rxjs';
 import { map } from 'rxjs';
 
 import { Pessoa } from '../models/pessoa.model';
